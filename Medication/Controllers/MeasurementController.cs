8a1883612afc4e565e5509f13860ce3a82b8d0b8
213547409 <213547409@PC5-1>
2023-03-12 18:43:46 +0200

    adding struct for measurement functunality

---
+++ Medication/Controllers/MeasurementController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Medication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeasurementController : ControllerBase
+    {
+        // GET: api/<MeasurementController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MeasurementController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MeasurementController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MeasurementController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MeasurementController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
